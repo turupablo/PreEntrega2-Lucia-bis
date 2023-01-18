29dcebb5302aef99ac125f34f37cd04525667f44
turupablo <turupablo@hotmail.com>
2023-01-17 23:49:36 -0300

    Mejora estetica

---
+++ src/components/Item.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const Item = ({ producto }) => {
+    return (
+        <article key={producto.id} className="product-card">
+            <div class="row justify-content-center p-5">
+                <div class="card shadow carta-contenedor bg-body rounded text-center ">
+                    <h6 className="product-card__title">{producto.title}</h6>
+                    <img className="product-card__image img-fluid border border-secondary" src={`https://picsum.photos/200/300?random=${producto.id}`} alt={producto.title} />
+                    <p class="text-secondary precio">${producto.price}</p>
+                    <Link to={"/item/"+producto.id}> <button class="btn btn-secondary">Mas</button> </Link>
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default Item
---
+++ src/components/ItemList.js
@@ -0,0 +1,15 @@
+import Item from "./Item"
+
+const ItemList = ({productos}) => {
+    return (
+        <section className="product-container container">
+            {productos.map((producto) => {
+                return (
+                    <Item producto={producto} key={producto.id}/>
+                )
+            })}
+        </section>
+    )
+}
+
+export default ItemList
